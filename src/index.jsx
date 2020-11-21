a5099921a5c2927779016aa4c666234e91220032
Barbora Petříková <bb.petrikova@gmail.com>
2020-11-21 16:22:36 +0100

    oprava importu

--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.html';
 import './style.css';
-import './jokes.js';
+import { jokes } from './jokes.js';
 import Joke from './Joke/Joke.jsx';
 
 const App = () => {
-let props = jokes.map((joke) => 
-  return (
-    <>
-      <Joke
-        userId={joke.id}
-        userName={joke.name}
-        text={joke.text}
-        likes={joke.likes}
-        dislikes={joke.dislikes}
-      />
-    </>
-  );
-)};
-
+  let props = jokes.map((joke) => {
+    return (
+      <>
+        <Joke
+          userId={joke.id}
+          userName={joke.name}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      </>
+    );
+  });
+  return props;
+};
 render(<App />, document.querySelector('#app'));
